c47640db62e59469c3ce4e9c2d9c9ae58dd79cf3
lobao <fabioslobao@hotmail.com>
2024-11-04 18:59:56 +0100

    Update install script to run independently of the existing environment.

--- FileCataloger/install/file_catalog.ps1
+++ FileCataloger/install/file_catalog.ps1
@@ -1,5 +1,6 @@
 # Constansts that control the script
-$CONDA_PATH = "C:\path\to\conda\Scripts\conda.exe" # Adjust this to your Conda installation path
+$VERSION = "1.0.0"
+$PROVIDER = "Anatel"
 $ENV_NAME = "fileCataloger"
 $REPETITION_INTERVAL = 60 # Minutes between each run of the service
 $TASK_NAME = "FileCatalogService"
@@ -7,18 +8,35 @@ $TASK_DESCRIPTION = "Regulatron file cataloging tool file_catalog.py."
 
 # Define download URLs
 $urls = @{
+    "Miniconda3-latest-Windows-x86_64.exe" = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe";
     "config.json" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/src/config.json";
     "file_catalog.py" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/src/file_catalog.py";
     "environment.yml" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/environment.yml"
 }
 
-# Define download directory
-$downloadDir = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("MyDocuments"), "Downloads", "FileCataloger")
+# Define download directory using current user download folder
+$downloadDir = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("MyDocuments"), "Downloads", $ENV_NAME)
 if (!(Test-Path -Path $downloadDir)) {
     New-Item -ItemType Directory -Path $downloadDir
 }
 
-# Download files from GitHub
+# Define the installation path using the Program Files directory
+$installPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("ProgramFiles"), $PROVIDER, $ENV_NAME)
+if (!(Test-Path -Path $installPath)) {
+    New-Item -ItemType Directory -Path $installPath
+}
+
+# Print splash screen
+$colunas = $Host.UI.RawUI.BufferSize.Width
+Write-Host "`n"
+Write-Host ("~" * $colunas) -ForegroundColor Green
+Write-Host "REGULATRON" -ForegroundColor Green
+Write-Host "Script de instalação de catalogador de arquivos" -ForegroundColor Green
+Write-Host "Versão: $VERSION" -ForegroundColor Green
+Write-Host ("~" * $colunas) -ForegroundColor Green
+Write-Host "`n"
+
+# Download required files from the URLs defined above
 foreach ($fileName in $urls.Keys) {
     $url = $urls[$fileName]
     $filePath = Join-Path -Path $downloadDir -ChildPath $fileName
@@ -26,14 +44,16 @@ foreach ($fileName in $urls.Keys) {
     Write-Output "Downloaded $fileName to $filePath"
 }
 
-$installPath = [System.Environment]::GetFolderPath("ProgramFiles")
-if (!(Test-Path -Path $installPath)) {
-    New-Item -ItemType Directory -Path $installPath
-}
+# Install Miniconda silently in the install directory
+& $downloadDir/Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=$installPath
+Write-Output "Miniconda installed to $installPath"
+
+# Define the path to the Conda executable
+$condaPath = [System.IO.Path]::Combine($installPath, "Miniconda3")
 
 # Create the Conda environment within the install directory
 Write-Output "Creating Conda environment..."
-& $CONDA_PATH create --prefix $installPath\$ENV_NAME --file $downloadDir\environment.yml -y
+& $condaPath create --name $ENV_NAME --file $downloadDir\environment.yml -y
 
 # Verify environment creation
 if (!(Test-Path "$installPath\$ENV_NAME")) {
@@ -42,12 +62,19 @@ if (!(Test-Path "$installPath\$ENV_NAME")) {
 }
 Write-Output "Conda environment '$ENV_NAME' created successfully."
 
+# move the config file and file_catalog.py to the install directory
+Move-Item -Path "$downloadDir\config.json" -Destination "$installPath\config.json"
+Move-Item -Path "$downloadDir\file_catalog.py" -Destination "$installPath\file_catalog.py"
+
+# Remove download folder
+Remove-Item -Path $downloadDir -Recurse
+
 # Create a batch file to run the Python script in the Conda environment
-$batchFilePath = Join-Path -Path $installPath -ChildPath "run_file_catalog.bat"
+$batchFilePath = [System.IO.Path]::Combine($installPath, "run_file_catalog.bat")
 $batchContent = @"
 @echo off
-call $condaPath activate $envName
-python $installPath\file_catalog.py
+call "$condaPath\Scripts\activate.bat" $ENV_NAME
+python "$installPath\file_catalog.py"
 "@
 $batchContent | Out-File -FilePath $batchFilePath -Encoding UTF8
 
