333125c2c974a7fef78957739125fca9afde644a
lobao <fabioslobao@hotmail.com>
2024-11-08 15:17:38 +0100

    Install files and script

--- FileCataloger/install/file_catalog.ps1
+++ FileCataloger/install/file_catalog.ps1
@@ -1,29 +1,18 @@
 # Constansts that control the script
 $VERSION = "1.0.0"
 $PROVIDER = "Anatel"
-$ENV_NAME = "fileCataloger"
+$ENV_NAME = "regulatronCataloger"
+$MINICONDA_PATH = "c:\ProgramData\miniconda3"
+$MINICONDA_URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
 $REPETITION_INTERVAL = 60 # Minutes between each run of the service
 $TASK_NAME = "FileCatalogService"
 $TASK_DESCRIPTION = "Regulatron file cataloging tool file_catalog.py."
 
 # Define download URLs
 $urls = @{
-    "Miniconda3-latest-Windows-x86_64.exe" = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe";
     "config.json" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/src/config.json";
-    "file_catalog.py" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/src/file_catalog.py";
+    "file_catalog.exe" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/src/file_catalog.py";
     "environment.yml" = "https://raw.githubusercontent.com/InovaFiscaliza/Tools/refs/heads/main/FileCataloger/environment.yml"
-}
-
-# Define download directory using current user download folder
-$downloadDir = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("MyDocuments"), "Downloads", $ENV_NAME)
-if (!(Test-Path -Path $downloadDir)) {
-    New-Item -ItemType Directory -Path $downloadDir
-}
-
-# Define the installation path using the Program Files directory
-$installPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("ProgramFiles"), $PROVIDER, $ENV_NAME)
-if (!(Test-Path -Path $installPath)) {
-    New-Item -ItemType Directory -Path $installPath
 }
 
 # Print splash screen
@@ -36,6 +25,26 @@ Write-Host "Versão: $VERSION" -ForegroundColor Green
 Write-Host ("~" * $colunas) -ForegroundColor Green
 Write-Host "`n"
 
+
+
+# Define, test and create download directory using current user download folder
+$downloadDir = [System.IO.Path]::Combine((New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path, $ENV_NAME)
+if (!(Test-Path -Path $downloadDir)) {
+    if (!(New-Item -ItemType Directory -Path $downloadDir)) {
+        Write-Output "Failed to create download directory. Exiting script."
+        exit 1
+    }
+}
+
+# Define, test and create the installation path using the Program Files directory
+$installPath = [System.IO.Path]::Combine($env:ProgramData, $PROVIDER, $ENV_NAME)
+if (!(Test-Path -Path $installPath)) {
+    if (!(New-Item -ItemType Directory -Path $installPath)) {
+        Write-Output "Failed to create installation directory. Exiting script."
+        exit 1
+    }
+}
+
 # Download required files from the URLs defined above
 foreach ($fileName in $urls.Keys) {
     $url = $urls[$fileName]
@@ -44,20 +53,29 @@ foreach ($fileName in $urls.Keys) {
     Write-Output "Downloaded $fileName to $filePath"
 }
 
-# Install Miniconda silently in the install directory
-& $downloadDir/Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=$installPath
-Write-Output "Miniconda installed to $installPath"
+#test if miniconda is already installed
+if (!(Test-Path -Path $MINICONDA_PATH)) {
+    # Download the Miniconda installer
+    $downloadFileName = "Miniconda3-latest-Windows-x86_64.exe"
+    $minicondaPath = Join-Path -Path $downloadDir -ChildPath $downloadFileName
+    Invoke-WebRequest -Uri $MINICONDA_URL -OutFile $
+    Write-Output "Downloaded Miniconda installer to $minicondaPath"
+
+    # Install Miniconda silently in the install directory
+    & $downloadDir/$downloadFileName /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=$MINICONDA_PATH
+    Write-Output "Miniconda installed to $MINICONDA_PATH"
+}
 
 # Define the path to the Conda executable
-$condaPath = [System.IO.Path]::Combine($installPath, "Miniconda3")
+$condaExecutable = [System.IO.Path]::Combine($MINICONDA_PATH, "Scripts\conda.exe")
 
 # Create the Conda environment within the install directory
 Write-Output "Creating Conda environment..."
 & $condaPath create --name $ENV_NAME --file $downloadDir\environment.yml -y
 
-# Verify environment creation
-if (!(Test-Path "$installPath\$ENV_NAME")) {
-    Write-Output "Conda environment creation failed. Exiting script."
+# Verify environment creation (last command output is stored in $?)
+if (-not $?) {
+    Write-Output "Failed to create Conda environment. Exiting script."
     exit 1
 }
 Write-Output "Conda environment '$ENV_NAME' created successfully."
